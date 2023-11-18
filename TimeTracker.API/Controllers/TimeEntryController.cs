52c129c0b3c78e0bcfeedc92b7a3189002edb4c5
Dean McCoy <dean92@gmail.com>
2023-11-18 10:53:35 -0600

    Add project files.

---
+++ TimeTracker.API/Controllers/TimeEntryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Shared.Entities;
+
+namespace TimeTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeEntryController : ControllerBase
+    {
+        private static List<TimeEntry> _timeEntries = new List<TimeEntry>
+        {
+            new TimeEntry
+            { Id = 1, Project = "TimeTracker app", End = DateTime.Now.AddHours(1) },
+        };
+
+        [HttpGet]
+        public ActionResult<List<TimeEntry>> GetAlTimeEntries()
+        {
+            return Ok(_timeEntries);
+        }
+    }
+
+    
+}
